75e0bc7ab5b6d524ed630b3532facec7a2bcce1a
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-10-13 00:44:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ export default function Home({ allPostsData }) {
   return (
     <div className="min-h-screen bg-dark-bg text-dark-text">
       <main className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-bold mb-8 text-dark-primary">My Blog</h1>
+        <h1 className="text-4xl font-bold mb-8 text-dark-primary">Tristan Poland - Blog</h1>
+        Here we learn things, we experiment, we crotique, and event get a little sarcastic now and then. I hope you enjoy my little writing corner of the internet!
+        <br></br><br></br>
         <div className="grid gap-6 md:grid-cols-2">
           {allPostsData.map(({ id, date, title }) => (
             <Link key={id} href={`/posts/${id}`} className="block bg-dark-surface rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 transform hover:scale-105 transition-transform duration-300">
