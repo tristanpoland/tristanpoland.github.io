3c7e009424c6e472eb988227674215d4a2e9c269
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-06-22 20:37:31 -0400

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,24 @@
 import '../styles/globals.css'
 import Footer from '../components/footer'
 import Nav from '../components/nav'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="dark flex flex-col min-h-screen">
-      <div className="flex-grow">
-      <Nav />
-      <div className="bg-black" style={{ height: '60px' }}></div>
-        <Component {...pageProps} />
+    <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/image/favicon.ico" />
+        <title>Tristan Poland - Systems Programmer, Game Dev, Cloud Engineer</title>
+      </Head>
+      <div className="dark flex flex-col min-h-screen">
+        <div className="flex-grow">
+          <Nav />
+          <div className="bg-black" style={{ height: '60px' }}></div>
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 
